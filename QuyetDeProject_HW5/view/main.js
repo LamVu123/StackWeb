0a0fe53b4d2d826ca601ef8692ad9ad64f1f4018
lamvu123 <lamvu.minsk89@gmail.com>
2019-01-03 00:23:22 +0700

    delete

--- QuyetDeProject_HW5/view/main.js
+++
@@ -1,5 +0,0 @@
-document.getElementById("Content").addEventListener("input", function() {
-    document.getElementById("count").innerText = 200 - document.getElementById("Content").value.length;
-    var check = 200 - document.getElementById("Content").value.length;
-    console.log(check);
-});
